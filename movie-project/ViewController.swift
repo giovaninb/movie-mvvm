5b0835d6036e02cd0c5a41f9e03bac55e41fe9fd
Giovani Bettoni <bettoni.gn@gmail.com>
2020-09-29 16:26:17 -0300

    Initial Commit

---
+++ movie-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movie-project
+//
+//  Created by Giovani Nícolas Bettoni on 29/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
